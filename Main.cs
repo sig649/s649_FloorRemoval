9d8005defb9b74c6cfc7b97b5493e5ecc633f2cd
sig649 <sig7alista@gmail.com>
2025-01-22 03:21:24 +0900

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,301 @@
+//using System;
+///using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+using UnityEngine;
+using BepInEx.Configuration;
+//using System.IO;
+//using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace s649VT
+{
+    [BepInPlugin("s649_VanillaTweaks", "s649 Vanilla Tweaks", "0.0.0.0")]
+
+    public class Main : BaseUnityPlugin
+    {
+       // private static ConfigEntry<bool> flagEnableLogging;
+
+        //public static bool propFlagEnablelLogging
+        //{
+            //get => flagEnableLogging.Value;
+            //set => flagEnableLogging = value;
+        //}
+
+        private void Start()
+        {
+            //flagEnableLogging = Config.Bind("#0General", "ENABLE_LOGGING", true, "Enable Logging");
+
+            //UnityEngine.Debug.Log("[LS]Start [configLog:" + propFlagEnablelLogging.ToString() + "]");
+            var harmony = new Harmony("Main");
+            new Harmony("Main").PatchAll();
+        }
+        //public static void Lg(string t)
+        //{
+        //    UnityEngine.Debug.Log(t);
+        //}
+        //public static bool IsOnGlobalMap(){
+        //    return (EClass.pc.currentZone.id == "ntyris") ? true : false;
+        //}
+    }
+    //++++EXE++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    [HarmonyPatch]
+    public class MapExe{
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Map), "MineFloor")]
+        public static bool Prefix(Map __instance, Point point, Chara c, bool recoverBlock, bool removePlatform){
+            string text = "[LS]MF [";
+            text += "Map:" + __instance.ToString() + "][";
+            text += "P:" + point.ToString() + "][";
+            text += "C:" + c.ToString() + "][";
+            text += "rB:" + recoverBlock.ToString() + "][";
+            text += "rP:" + removePlatform.ToString() + "][";
+            text += "]";
+            if(point.sourceFloor.id == 4){
+                //Debug.Log("Floor is hatake");
+                return false;
+            }
+            return true;
+            //Debug.Log(text);
+        }
+        /*
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Map), "DropBlockComponent")]
+        public static void Postfix(Point point,TileRow r,SourceMaterial.Row mat, bool recoverBlock, bool isPlatform, Chara c){
+            string text = "[LS]DBC [";
+            //text += "Map:" + __instance.ToString() + "][";
+            text += "P:" + point.ToString() + "][";
+            text += "r:" + r.ToString() + "][";
+            text += "rid:" + r.id.ToString() + "][";
+            text += "mat:" + mat.ToString() + "][";
+            text += "rB:" + recoverBlock.ToString() + "][";
+            text += "iP:" + isPlatform.ToString() + "][";
+            //text += "c:" + c.ToString() + "][";
+            text += "]";
+            Debug.Log(text);
+        }
+        */
+        /*
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ThingGen), "CreateRawMaterial")]
+        public static void Postfix(SourceMaterial.Row row){
+            Debug.Log("[LS]TG->CRM : " + row.ToString());
+        }*/
+    }
+}
+//------------template--------------------------------------------------------------------------------------------
+/*
+[HarmonyPatch]
+
+[HarmonyPrefix]
+[HarmonyPostfix]
+
+[HarmonyPatch(typeof(----),"method")]
+public class ------{}
+
+public static void ----(type arg){}
+public static bool Pre--(type arg){}
+
+[HarmonyPatch]
+public class PreExe{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(------), "+++++++")]
+    public static bool Prefix(type arg){}
+}
+
+[HarmonyPatch]
+public class PostExe{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(------), "+++++++")]
+    public static void Postfix(type arg){}
+}
+
+*/
+
+//////trash box//////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/*
+    [HarmonyPatch(typeof(Zone))]
+    [HarmonyPatch(nameof(Zone.Activate))]
+    public class HarmonyAct
+    {
+        //[HarmonyPostfix]
+        
+        static void Postfix(Zone __instance)
+        {
+            Zone z = __instance;
+            Zone topZo = z.GetTopZone();
+            FactionBranch br = __instance.branch;
+            //Lg("[LS]Fooked!");
+            if (Main.propFlagEnablelLogging)
+            {
+                Lg("[LS]CALLED : Zone.Activate ");
+                string text;
+
+                text = ("[LS]Ref : [Z:" + z.id.ToString() + "]");
+                //text += (" [id:" + z.id.ToString() + "]");
+                text += (" [Dlv:" + z.DangerLv.ToString() + "]");
+                text += (" [blv:" + Mathf.Abs(z.lv).ToString() + "]");
+                text += (" [bDLV:" + z._dangerLv.ToString() + "]");
+                text += (" [Dlfi:" + z.DangerLvFix.ToString() + "]");
+                if(topZo != null && z != topZo){text += (" [tpZ:" + topZo.NameWithLevel + "]");}
+                if(br != null){text += (" [br:" + br.ToString() + "]");}
+                if(z.ParentZone != null && z != z.ParentZone)text += (" [PaZ: " + z.ParentZone.id.ToString() + "]") ;
+                 text += (" [Pce:" + z.isPeace.ToString() + "]");
+                 text += (" [Twn:" + z.IsTown.ToString() + "]");
+                Lg(text);
+                //text = ("[LS]Charas : " + EClass._map.charas.Count);
+                //text += (" [Stsn:" + z.isPeace.ToString() + "]");
+            }
+            
+        }
+        public static void Lg(string t)
+        {
+            UnityEngine.Debug.Log(t);
+        }
+        
+    }
+
+    [HarmonyPatch(typeof(HotItem))]
+    public class HotPatch {
+        //[HarmonyPrefix]
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(HotItem.TrySetAct))]
+        static void FookPostExe(HotItem __instance){
+            //Debug.Log("[LS]Fooking->" + __instance.ToString());
+        }
+    }  
+
+    [HarmonyPatch]
+    public class TickPatch{
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Chara),"TickConditions")]
+        public static void TickExe(Chara __instance){
+            Chara c = __instance;
+            if(c.IsPC){
+                //Debug.Log("[LS]QuestMain : " + QuestMain.Phase.ToString());
+            }
+        }
+    }
+    */
+/*
+    [HarmonyPatch(typeof(TraitDoor))]
+    public class PatchAct2 {
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(TraitDoor.TryOpen))]
+        static void FookPreExe(TraitDoor __instance, Chara c, ref bool __state){
+            __state = __instance.IsOpen() ? true : false;
+            //if(c.IsPC){ Lg("[LS]TraitDoor.TryOpen Called! by->" + c.ToString());}
+            
+            
+        }
+        
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(TraitDoor.TryOpen))]
+        static void FookPostExe(TraitDoor __instance, Chara c, bool __state){
+            if(!__state && __instance.IsOpen()){
+                if(c.IsPC){ 
+                    //Lg("[LS]TraitDoor.Close->Open!" + c.ToString());
+                    }
+            }
+           
+        }
+        
+        
+        public static void Lg(string t)
+        {
+            UnityEngine.Debug.Log(t);
+        }
+    }
+   
+public class Main : BaseUnityPlugin {
+    private void Start() {
+        var harmony = new Harmony("NerunTest");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(Zone))]
+[HarmonyPatch(nameof(Zone.Activate))]
+class ZonePatch {
+    static void Prefix() {
+        Debug.Log("Harmoney Prefix");
+    }
+    static void Postfix(Zone __instance) {
+        Debug.Log("Harmoney Postfix");
+        }
+}
+*/
+
+/*
+        
+        */
+        /*
+        [HarmonyPatch(typeof(Zone))]
+        [HarmonyPatch(nameof(Zone.Activate))]
+        class ZonePatch {
+        static void Postfix(Zone __instance) {
+            Lg("[LS]CALLED : Zone.Activate " + __instance.ToString());
+            Lg("[LS]Zone : [DLV : " + __instance.DangerLv.ToString() + "]");
+            Lg("[LS]Player : [dst : " + EClass.player.stats.deepest.ToString() + "]");
+            }
+        }
+        */
+
+        /*
+        static void PostMoveZone(Player __instance)
+        {
+            Lg("[LS]Fooked!MoveZone!");
+            if (Main.propFlagEnablelLogging.Value)
+            {
+                int dst = EClass.player.stats.deepest;
+                Lg("[LS]CALLED : Player.MoveZone ");
+                Lg("[LS]Player : [dst : " + dst.ToString() + "]");
+            }
+        }
+
+        */
+
+
+        /*
+        [HarmonyPatch(typeof(Card), "AddExp")]
+        
+        class CardPatch
+        {
+            [HarmonyPrefix]
+            static bool AddExpHook(Card __instance)
+            {
+                Lg("[LS]Fooked:AddExp!");
+                
+                if (Main.propFlagEnablelLogging.Value)
+                {
+                    if(__instance.IsPC){
+                        Lg("[LS]Card : [name : " + __instance.ToString() + "]");
+                    }
+                    //Lg("[LS]Card : [name : " + dst.ToString() + "]");
+                    //Lg("[LS]Player : [dst : " + dst.ToString() + "]");
+                }
+                return true;
+            }
+        }
+        */
+
+        /*
+        [HarmonyPatch(typeof(Zone), "DangerLv", MethodType.Getter)]
+        class ZonePatch {
+            [HarmonyPrefix]
+            static bool Prefix(Zone __instance) {
+                Lg("[LS]CALLED : Zone.DangerLV ");
+                //Lg("[LS]Zone : [Z.toSt : " + __instance.ToString() + "]");
+                //Lg("[LS]Zone : [DLV : " + __instance.DangerLv.ToString() + "]");
+                //Lg("[LS]Player : [dst : " + EClass.player.stats.deepest.ToString() + "]");
+                return true;
+            }
+        }
+        */
